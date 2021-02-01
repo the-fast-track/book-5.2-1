153443eb0cbb3e81ca938d786b9a98d247b7f064
Fabien Potencier <fabien@potencier.org>
2021-02-01 16:41:51 +0100

    Add 9.1: Configuring EasyAdmin

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -44,6 +44,11 @@ class Conference
         $this->comments = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->city.' '.$this->year;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
