f7cba6784d4a13ed12d908c466509272142c560d
Fabien Potencier <fabien@potencier.org>
2021-02-01 16:43:54 +0100

    Add 13.1: Defining Lifecycle Callbacks

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Comment
 {
@@ -104,6 +105,14 @@ class Comment
         $this->createdAt = $createdAt;
 
         return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTime();
     }
 
     public function getConference(): ?Conference
