b4bcdb4cf285302cfd67cfa47b47fca109ea3422
Fabien Potencier <fabien@potencier.org>
2021-02-01 17:13:26 +0100

    Add 22.2: Leveraging Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import 'bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
 
 // start the Stimulus application
 import './bootstrap';
+
+bsCustomFileInput.init();
