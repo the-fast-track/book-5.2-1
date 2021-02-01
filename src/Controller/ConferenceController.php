335067d062aaeeb4d6fd6facf06adaa7eb19422c
Fabien Potencier <fabien@potencier.org>
2021-02-01 17:11:58 +0100

    Add 21.1: Adding HTTP Cache Headers

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -33,9 +33,12 @@ class ConferenceController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response($this->twig->render('conference/index.html.twig', [
+        $response = new Response($this->twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
