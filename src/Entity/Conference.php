9635259e351f046ccc68a188502a106ed82f7753
Fabien Potencier <fabien@potencier.org>
2021-02-05 07:18:44 +0100

    Add 26.2: Exposing an API for Conferences

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,16 +2,25 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
  * @UniqueEntity("slug")
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="conference:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="conference:item"}}},
+ *     order={"year"="DESC", "city"="ASC"},
+ *     paginationEnabled=false
+ * )
  */
 class Conference
 {
@@ -20,21 +29,25 @@ class Conference
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $city;
 
     /**
      * @ORM\Column(type="string", length=4)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $year;
 
     /**
      * @ORM\Column(type="boolean")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $isInternational;
 
     /**
@@ -45,6 +58,7 @@ class Conference
     /**
      * @ORM\Column(type="string", length=255, unique=true)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $slug;
 
     public function __construct()
