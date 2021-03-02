fd94533ee6da462176de3fe9cefc349009a16c1b
Marko Kaznovac <kaznovac@gmail.com>
2021-03-02 17:04:03 +0100

    remove spa/node_modules

--- spa/node_modules/assets-webpack-plugin/dist/lib/getFileExtension.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function getFileExtension(asset) {
-  var extRegex = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/i;
-  var ext = asset.match(extRegex);
-
-  return ext ? ext[0].slice(1) : '';
-};
